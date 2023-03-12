c74d7e86da961afcc85f848101849abbd7b17add
vai-xyz <119789706+vai-xyz@users.noreply.github.com>
2023-03-12 16:34:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preLoad(){
+}
+
+function setup(){
+    canvas=createCanvas(4640,480);
+    canvas.position(120,250);
+    video=createCapture(VIDEO);
+    video.hide();
+
+    tint_color="";
+}
+
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('student_activity.png');
+
+}
+function filter_tint(){
+tint_color=document.getElementById("color_name").value;
+}
